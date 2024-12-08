306b70e803bc8d50b919c7ac557e11b9bf1f921c
pkoevesdi <github.domain.gauner@spamgourmet.com>
2024-12-08 15:24:58 +0100

    changed to different board type fixed several issues with the original `flash-algorithm-template`

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Flash Algorithm for STM32h730 with external NOR flash IS25LP128
-A flash algorithm using the https://github.com/probe-rs/flash-algorithm-template.
+# Flash Algorithm for STM32h750 with dual external NOR flash MT25QL512ABB  
+
+This is a flash algorithm for usage with `probe-rs`.
+Works with the current (2024-11-30) `master` of `probe-rs`, which can be intalled with `cargo install probe-rs-tools target-gen --git https://github.com/probe-rs/probe-rs --locked --force`
+
+It is instantiated by
+```bash
+cargo generate gh:probe-rs/flash-algorithm-template --name=stm32h750xx_mt25_ql512 \
+-d target-arch=thumbv7em-none-eabihf \
+-d ram-start-address=0x20000000 \
+-d ram-size=0x20000 \
+-d flash-start-address=0x90000000 \
+-d flash-size=0x8000000 \
+-d flash-page-size=0x100 \
+-d flash-sector-size=0x10000 \
+-d empty-byte-value=0xff
+```
+
+## Developing the algorithm
+
+Just run `cargo run`. It spits out the flash algo in the probe-rs YAML format and downloads it onto a target and makes a test run.
+You will also be able to see RTT messages.
+
+You can find the generated YAML in `target/definition.yaml`.
---
+++ src/main.rs
@@ -0,0 +1,127 @@
+#![no_std] // from template
+#![no_main] // from template
+
+use flash_algorithm::*; // from template
+use rtt_target::{rprintln, rtt_init_print}; // from template
+
+use stm32h7xx_hal::{pac, prelude::*};
+
+struct Algorithm; // from template
+
+// pub struct Algorithm {
+//     quadspi: Qspi<QUADSPI>,
+// }
+
+// from initialization:
+algorithm!(Algorithm, {
+    flash_address: 0x90000000,
+    flash_size: 0x8000000,
+    page_size: 0x100,
+    empty_value: 0xFF,
+    sectors: [{
+        size: 0x10000,
+        address: 0x0,
+    }]
+});
+
+// Rest: empty functions from template
+impl FlashAlgorithm for Algorithm {
+    fn new(_address: u32, _clock: u32, _function: Function) -> Result<Self, ErrorCode> {
+        rtt_init_print!(); // from template
+        rprintln!("Init"); // from template
+
+        let dp = pac::Peripherals::take().unwrap();
+
+        // Constrain and Freeze power
+        let pwr = dp.PWR.constrain();
+        let pwrcfg = pwr.freeze();
+
+        // Constrain and Freeze clock
+        let rcc = dp
+            .RCC
+            .constrain()
+            .use_hse(1.MHz()) // use (and thus test) external clock - "Will result in a hang if an external oscillator is not connected or it fails to start." - https://docs.rs/stm32h7xx-hal/latest/stm32h7xx_hal/rcc/struct.Rcc.html#method.use_hse
+            .sys_ck(64.MHz());
+
+        rprintln!("            Freezing the core clocks...");
+        let _ccdr = rcc.freeze(pwrcfg, &dp.SYSCFG);
+
+        // rprintln!("            hse_ck: {}", ccdr.clocks.hse_ck().unwrap());
+        // rprintln!("            sys_ck: {}", ccdr.clocks.sys_ck());
+        // rprintln!("            hclk: {:}", ccdr.clocks.hclk());
+
+        // let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
+        // let gpioc = dp.GPIOC.split(ccdr.peripheral.GPIOC);
+        // let gpiod = dp.GPIOD.split(ccdr.peripheral.GPIOD);
+        // let gpioe = dp.GPIOE.split(ccdr.peripheral.GPIOE);
+
+        // // "All GPIOs have to be configured in very high-speed configuration." - AN5050, p. 30
+        // let clk = gpiob.pb2.into_alternate::<9>().speed(Speed::VeryHigh);
+        // let _bk1_ncs = gpiob.pb6.into_alternate::<10>().speed(Speed::VeryHigh);
+        // let _bk2_ncs = gpioc.pc11.into_alternate::<9>().speed(Speed::VeryHigh);
+        // let bk1_io0 = gpiod.pd11.into_alternate::<9>().speed(Speed::VeryHigh);
+        // let bk1_io1 = gpiod.pd12.into_alternate::<9>().speed(Speed::VeryHigh);
+        // let bk1_io2 = gpioe.pe2.into_alternate::<9>().speed(Speed::VeryHigh);
+        // let bk1_io3 = gpiod.pd13.into_alternate::<9>().speed(Speed::VeryHigh);
+        // let _bk2_io0 = gpioe.pe7.into_alternate::<10>().speed(Speed::VeryHigh);
+        // let _bk2_io1 = gpioe.pe8.into_alternate::<10>().speed(Speed::VeryHigh);
+        // let _bk2_io2 = gpioe.pe9.into_alternate::<10>().speed(Speed::VeryHigh);
+        // let _bk2_io3 = gpioe.pe10.into_alternate::<10>().speed(Speed::VeryHigh);
+
+        // // Initialise the OCTOSPI peripheral.
+        // let mut quadspi = dp.QUADSPI.bank1(
+        //     (clk, bk1_io0, bk1_io1, bk1_io2, bk1_io3),
+        //     75.MHz(),
+        //     &ccdr.clocks,
+        //     ccdr.peripheral.QSPI,
+        // );
+
+        // switch to QPI mode
+        // octospi
+        //     .write_extended(
+        //         OctospiWord::U8(cmds::Cmds::Qpien as u8),
+        //         OctospiWord::None,
+        //         OctospiWord::None,
+        //         &[],
+        //     )
+        //     .unwrap();
+
+        // quadspi
+        //     .inner_mut()
+        //     .dcr
+        //     .modify(|_, w| unsafe { w.fsize().bits(26) }); // set flash size to 2^27 bytes (= 2*512 Mbit)
+
+        // // Change bus mode
+        // quadspi.configure_mode(QspiMode::OneBit).unwrap();
+
+        rprintln!("            done.");
+
+        // Ok(Self { quadspi })
+
+        Ok(Self) // from template
+    }
+
+    fn erase_all(&mut self) -> Result<(), ErrorCode> {
+        rprintln!("Erase All");// from template
+        // TODO: Add code here that erases the entire flash.
+        Err(ErrorCode::new(0x70d0).unwrap())// from template
+    }
+
+    fn erase_sector(&mut self, addr: u32) -> Result<(), ErrorCode> {
+        rprintln!("Erase sector addr:{}", addr);// from template
+        // TODO: Add code here that erases a page to flash.
+        Ok(())// from template
+    }
+
+    fn program_page(&mut self, addr: u32, data: &[u8]) -> Result<(), ErrorCode> {
+        rprintln!("Program Page addr:{} size:{}", addr, data.len());// from template
+        // TODO: Add code here that writes a page to flash.
+        Ok(())// from template
+    }
+}
+
+impl Drop for Algorithm {
+    fn drop(&mut self) {
+        // TODO: Add code here to uninitialize the flash algorithm.
+    }
+}
